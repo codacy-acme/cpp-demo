a600c460f701eb463fef0c3b93be8e6d3c0c5d6e
heliocodacy <46491761+heliocodacy@users.noreply.github.com>
2020-11-06 10:53:51 +0000

    Update test.js

--- test.js
+++ test.js
@@ -7,6 +7,6 @@ setInterval(function() {
     var node = document.getElementById('Node');
     if(node) {
         // Do stuff with node and someResource.
-        node.innerHTML = JSON.stringify(someResource));
+        node.innerHTML = JSON.stringify(someResource);
     }
 }, 1000);
