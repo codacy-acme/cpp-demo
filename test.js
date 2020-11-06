961722345a921e80cf4be4ce67596452d3d1b857
heliocodacy <46491761+heliocodacy@users.noreply.github.com>
2020-11-06 10:59:23 +0000

    Update test.js

--- test.js
+++ test.js
@@ -10,3 +10,42 @@ setInterval(function() {
         node.innerHTML = JSON.stringify(someResource);
     }
 }, 1000);
+
+
+var elements = {
+    button: document.getElementById('button'),
+    image: document.getElementById('image'),
+    text: document.getElementById('text')
+};
+
+function doStuff() {
+    image.src = 'http://some.url/image';
+    button.click();
+    console.log(text.innerHTML);
+    // Much more logic
+}
+
+function removeButton() {
+    // The button is a direct child of body.
+    document.body.removeChild(document.getElementById('button'));
+
+    // At this point, we still have a reference to #button in the global
+    // elements dictionary. In other words, the button element is still in
+    // memory and cannot be collected by the GC.
+}
+
+var theThing = null;
+var replaceThing = function () {
+  var originalThing = theThing;
+  var unused = function () {
+    if (originalThing)
+      console.log("hi");
+  };
+  theThing = {
+    longStr: new Array(1000000).join('*'),
+    someMethod: function () {
+      console.log(someMessage);
+    }
+  };
+};
+setInterval(replaceThing, 1000);
