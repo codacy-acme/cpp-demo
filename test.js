d709fcfb7bb271fc084137a6d17f9b23db00e729
heliocodacy <46491761+heliocodacy@users.noreply.github.com>
2020-11-06 10:51:36 +0000

    Create test.js

---
+++ test.js
@@ -0,0 +1,12 @@
+function getData() {
+  return {}
+}
+
+var someResource = getData();
+setInterval(function() {
+    var node = document.getElementById('Node');
+    if(node) {
+        // Do stuff with node and someResource.
+        node.innerHTML = JSON.stringify(someResource));
+    }
+}, 1000);
